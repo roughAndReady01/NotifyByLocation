9707d9d656638683fff9c0632942a1b90a2df764
Haruki Hanagata <haruzo28@icloud.com>
2023-01-16 10:08:57 +0900

    Initial Commit

---
+++ NotifyByLocation/NotifyByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifyByLocationApp.swift
+//  NotifyByLocation
+//
+//  Created by 花形春輝 on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct NotifyByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
